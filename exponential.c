9f043b726166e26b5e6404f90e732f1870f1fefd
EmmaWagner <33786566+EmmaWagner@users.noreply.github.com>
2017-11-21 10:15:21 +0100

    Add files via upload

--- exponential.c
+++ exponential.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
- 
-int main() {
 
+
+double exponential(double x, double epsilon);
+
+int main() {
+    double x, e;
+    
+    printf("Bitte geben Sie x ein: ");
+    scanf("%lf", &x);
+    
+    printf("Bitte geben Sie Epsilon ein: ");
+    scanf("%lf", &e);
+    
+    printf("Ergebnis: %lf \nFehler: %lf\n",exponential(x,e), fabs(pow(M_E, x) - exponential(x,e)));
+}
+
+double exponential(double x, double epsilon) {
+// function exponential
 }
